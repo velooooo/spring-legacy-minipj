c23288c3d5ae2b7a9bc98b7cb005c912e8e60a1e
velooooo <wasupvelo@gmail.com>
2021-08-11 19:14:32 +0900

    sts_원본들 집파일로 저장 / sts_work폴더에 원본 통합으로 저장 / sts_work_home폴더에 개인프로젝트 작업 계속

---
+++ sts_work/oracleboard/src/main/java/co/kr/oracleboard/dto/BoardDTO.java
@@ -0,0 +1,111 @@
+package co.kr.oracleboard.dto;
+
+import java.sql.Timestamp;//년월일시분초 처리
+
+public class BoardDTO {
+
+	
+
+	//변수:필드:프로퍼티:property:속성
+	
+	private int bNum;
+	private String bName;
+	private String bTitle;
+	private String bContent;
+	private Timestamp bDate;
+	private int bHit;
+	private int bGroup;
+	private int bStep;
+	private int bIndent;
+	
+	//기본생성자
+	public BoardDTO() {}
+
+	//인자있는생성자 자동생성
+	public BoardDTO(int bNum, String bName, String bTitle, String bContent, Timestamp bDate, int bHit, int bGroup,
+			int bStep, int bIndent) {
+		super();
+		this.bNum = bNum;
+		this.bName = bName;
+		this.bTitle = bTitle;
+		this.bContent = bContent;
+		this.bDate = bDate;
+		this.bHit = bHit;
+		this.bGroup = bGroup;
+		this.bStep = bStep;
+		this.bIndent = bIndent;
+	}
+	
+	//getter / setter
+	public int getbNum() {
+		return bNum;
+	}
+
+	public void setbNum(int bNum) {
+		this.bNum = bNum;
+	}
+
+	public String getbName() {
+		return bName;
+	}
+
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+
+	public String getbTitle() {
+		return bTitle;
+	}
+
+	public void setbTitle(String bTitle) {
+		this.bTitle = bTitle;
+	}
+
+	public String getbContent() {
+		return bContent;
+	}
+
+	public void setbContent(String bContent) {
+		this.bContent = bContent;
+	}
+
+	public Timestamp getbDate() {
+		return bDate;
+	}
+
+	public void setbDate(Timestamp bDate) {
+		this.bDate = bDate;
+	}
+
+	public int getbHit() {
+		return bHit;
+	}
+
+	public void setbHit(int bHit) {
+		this.bHit = bHit;
+	}
+
+	public int getbGroup() {
+		return bGroup;
+	}
+
+	public void setbGroup(int bGroup) {
+		this.bGroup = bGroup;
+	}
+
+	public int getbStep() {
+		return bStep;
+	}
+
+	public void setbStep(int bStep) {
+		this.bStep = bStep;
+	}
+
+	public int getbIndent() {
+		return bIndent;
+	}
+
+	public void setbIndent(int bIndent) {
+		this.bIndent = bIndent;
+	}
+}
