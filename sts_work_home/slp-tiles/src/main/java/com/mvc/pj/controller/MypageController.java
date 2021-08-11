edbb457aaae45828ccbb64541b31db5ae4ad5b97
velooo <wasupvelo@gmail.com>
2021-08-11 13:10:04 +0900

    레이아웃 변경 완료/ member, board컨트롤러 수정/ 기타 서브페이지 view폴더파일과 controller추가 / slick slider추가 / 아이디 중복 검증이 제대로 이루어지지않는 문제 있음.

---
+++ sts_work_home/slp-tiles/src/main/java/com/mvc/pj/controller/MypageController.java
@@ -0,0 +1,18 @@
+package com.mvc.pj.controller;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MypageController {
+	@Autowired
+	private SqlSession sqlSession;//º¯¼ö
+	
+	
+	@RequestMapping("mypage")
+	public String mm() {
+		return ".main.member.mypage";//ºä ¸®ÅÏ
+	}
+}
